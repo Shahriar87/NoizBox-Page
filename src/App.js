cdbfa4746dd9efd1b4c34aba4e20bc46dc2fa1b5
Shahriar Firoz <shahriar.firoz@gmail.com>
2019-11-18 00:48:11 -0600

    Frontend API entry points added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate } from './components/Videos/Posts';
 import {Videos} from './components/Videos/Videos';
-import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
 import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
 import Dashboard from './components/CustomerView/Dashboard';
 import authProvider from './authProvider';
+import dataProvider from './components/dataProvider/rest';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
     <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
